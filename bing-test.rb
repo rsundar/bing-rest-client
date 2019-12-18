7beb59a7bf5126f58e4d910f4a067b37cd81cc66
rsundar <rohan.s.1987@gmail.com>
2019-12-18 18:55:04 +0530

    Added a set of tests for the bing rest client whoch we would need to get to green to complete the project.

---
+++ bing-test.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+
+describe "Test if the client is able to make a request to the bing server" do
+    
+end
+
+describe "Test if the query passed by the client produces a non-empty response" do
+    
+end
+
+describe "Test if the query microverse produces the expected response" do
+    
+end

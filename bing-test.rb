60a89cae1a380ba50bddb82bcebe36fb7a16ffd3
rsundar <rohan.s.1987@gmail.com>
2019-12-18 19:04:05 +0530

    Redefined the tests to more accurately represent the features of the project

--- bing-test.rb
+++ bing-test.rb
@@ -1,13 +1,13 @@
 require 'rspec'
 
-describe "Test if the client is able to make a request to the bing server" do
+describe Status do
     
 end
 
-describe "Test if the query passed by the client produces a non-empty response" do
+describe Non-Empty do
     
 end
 
-describe "Test if the query microverse produces the expected response" do
+describe Query do
     
 end

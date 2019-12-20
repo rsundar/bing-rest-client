b579c410b0e7062cd1abba43a3b4af9652132df4
rsundar <rohan.s.1987@gmail.com>
2019-12-20 20:46:37 +0530

    Added tests for the functionality of the rest-client

--- spec/bing-test.rb
+++ spec/bing-test.rb
@@ -1,13 +1,26 @@
 require 'rspec'
+require_relative '../bing.rb'
 
-describe Status do
-    
-end
+describe Bing do
+    let(:url) { "https://www.bing.com/search" }
+    let(:query) { "microverse" }
+    subject(:bing) {Bing.new(url,query)}
 
-describe Non-Empty do
+    describe Status do
+        it "The response code for the request should be 200" do
+            expect(bing.response_code).to eq(200)
+        end
+    end
     
-end
-
-describe Query do
+    describe Non-Empty do
+        it "Checks if the response body is non empty" do
+            expect(bing.body.nil?).to be false
+        end
+    end
     
+    describe Query do
+        it "Checks if the query string is present in the body" do
+            expect(bing.body.include?(query)).to be true
+        end
+    end
 end
